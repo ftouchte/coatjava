0874b93e65cf77bc16215067ac593311dc97594b
Felix Touchte Codjo <felixtouchtecodjo@gmail.com>
2026-05-07 11:31:41 -0400

    remove hit at more than 1.5 mm

--- reconstruction/alert/src/main/java/org/jlab/service/alert/ALERTEngine.java
+++ reconstruction/alert/src/main/java/org/jlab/service/alert/ALERTEngine.java
@@ -288,7 +288,7 @@ public class ALERTEngine extends ReconstructionEngine {
         double magfieldfactor = runBank.getFloat("solenoid", 0);
         double magfield = 50*magfieldfactor;
         PDGParticle proton = PDGDatabase.getParticleById(2212);
-        int Niter = 40;
+        int Niter = 35;
         KalmanFilter KF = new KalmanFilter(proton, Niter);
         
         // ------tmp propagation without a fit
@@ -304,21 +304,21 @@ public class ALERTEngine extends ReconstructionEngine {
         KF.propagation(AHDC_tracks, magfield, IsMC);
 
         /// Clean AHDC bad hits
-        // double sigma = 0.5; // mm
-        // for (Track track : AHDC_tracks) {
-        //     ArrayList<Hit> AHDC_hits = track.getHits();
-        //     Iterator<Hit> it = AHDC_hits.iterator();
-        //     while (it.hasNext()) {
-        //         Hit hit = it.next();
-        //         if (Math.abs(hit.getResidual()) > 3*sigma) {
-        //             it.remove();
-        //         }
-        //     }
-        // }
+        double sigma = 0.5; // mm
+        for (Track track : AHDC_tracks) {
+            ArrayList<Hit> AHDC_hits = track.getHits();
+            Iterator<Hit> it = AHDC_hits.iterator();
+            while (it.hasNext()) {
+                Hit hit = it.next();
+                if (Math.abs(hit.getResidual()) > 3*sigma) {
+                    it.remove();
+                }
+            }
+        }
 
         // /// Second propagation : each AHDC_tracks will be fitted
-        // KF.set_Niter(15);
-        // KF.propagation(AHDC_tracks, magfield, IsMC);
+        KF.set_Niter(10);
+        KF.propagation(AHDC_tracks, magfield, IsMC);
 
         /// write the AHDC::kftrack bank in the event
         event.removeBank("AHDC::kftrack");
